6d698cc7b0e862e57e6939f05ddffb00e1c53335
Tyler Savery <tyler@theyoungastronauts.com>
2025-06-17 10:35:19 -0400

    bio bolds

--- src/lib/data/bio.ts
+++ src/lib/data/bio.ts
@@ -2,16 +2,16 @@
 
 
 const bioExcerpt = `
-Hi, I'm Tyler Savery — a full-stack software developer based in Toronto, Canada. I've been coding since I was a kid and have spent the last 15 years building professional-grade digital products. I specialize in helping startups define and launch their MVPs, and I love turning ambitious ideas into polished, scalable platforms.
+Hi, I'm **Tyler Savery** — a full-stack software developer based in Toronto, Canada. I've been coding since I was a kid and have spent the last 15 years building professional-grade digital products. I specialize in helping startups define and launch their MVPs, and I love turning ambitious ideas into polished, scalable platforms.
 
 `;
 
 const fullBio = `
-Hi, I'm Tyler Savery — a full-stack software developer based in Toronto, Canada. I've been coding since I was a kid and have spent the last 15 years building professional-grade digital products. I specialize in helping startups define and launch their MVPs, and I love turning ambitious ideas into polished, scalable platforms.
+Hi, I'm **Tyler Savery** — a full-stack software developer based in Toronto, Canada. I've been coding since I was a kid and have spent the last 15 years building professional-grade digital products. I specialize in helping startups define and launch their MVPs, and I love turning ambitious ideas into polished, scalable platforms.
 
 Much of my development work happens through my agency, [The Young Astronauts](https://theyoungastronauts.com), which operates between Toronto and LA. We've collaborated with clients across entertainment, e-commerce, and tech startups, always with an eye toward thoughtful UX and modern, maintainable code.
 
-My current toolkit includes Python + Django, Dart + Flutter, and JavaScript + SvelteKit — though I'm always refining my stack to stay efficient and current. I also have a strong passion for UX design and, thanks to tools like Tailwind CSS, I can occasionally make things look as good as they work.
+My current toolkit includes **Python** + **Django**, **Dart** + **Flutter**, and **JavaScript** + **SvelteKit** — though I'm always refining my stack to stay efficient and current. I also have a strong passion for UX design and, thanks to tools like Tailwind CSS, I can occasionally make things look as good as they work.
 
 I'm equally passionate about sharing knowledge. My [YouTube](https://www.youtube.com/@TylerCodes)  channel covers both beginner-friendly and advanced topics in full-stack development, and it's a great resource if you're looking to level up your skills.
 
